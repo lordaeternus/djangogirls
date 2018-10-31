9410e5374fda60bde8b4ba2647157af1a0bd709d
lordaeternus <lordaeternus@gmail.com>
2018-10-31 11:37:43 -0300

    hoje

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-def olamundo(Request):
-    return True
+def post_list(request):
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts, 'nome':'Luciano'})
